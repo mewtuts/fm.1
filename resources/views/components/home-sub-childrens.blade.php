2404bce0316f02970b344699a54157f2722fd3e9
nathanielteria <102508838+nathanielteria@users.noreply.github.com>
2023-04-22 22:05:31 +0800

    update

--- resources/views/components/home-sub-childrens.blade.php
+++ resources/views/components/home-sub-childrens.blade.php
@@ -13,29 +13,31 @@
     <!--Uploaded files-->
     @foreach ($category->getFiles as $file)
         <div class="subParentFiles hidden">
-            <div class="text-base ml-10 cursor-pointer flex items-center w-fit justify-center">
-                @if ($file->file_type == 'jpeg' || $file->file_type == 'jpg' || $file->file_type == 'png' || $file->file_type == 'gif')
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @elseif ($file->file_type == 'docx')
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @elseif ($file->file_type == 'xlsx')
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @elseif ($file->file_type == 'ppt' || $file->file_type == 'pptx')
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @elseif ($file->file_type == 'pdf')
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @else
-                    <i class="bi bi-dot text-3xl text-green-800"></i>
-                @endif
+            <div class="hover:bg-slate-300 w-full">
+                <div class="text-base ml-10 cursor-pointer flex items-center w-fit justify-center">
+                    @if ($file->file_type == 'jpeg' || $file->file_type == 'jpg' || $file->file_type == 'png' || $file->file_type == 'gif')
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @elseif ($file->file_type == 'docx')
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @elseif ($file->file_type == 'xlsx')
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @elseif ($file->file_type == 'ppt' || $file->file_type == 'pptx')
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @elseif ($file->file_type == 'pdf')
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @else
+                        <i class="bi bi-dot text-3xl text-green-800"></i>
+                    @endif
 
-                @switch($file->file_type)
-                    @case('url')
-                        <p class="text-green-800 text-lg text-center"><a href="{{ $file->url }}" target="_blank"> {{ $file->alternative_name }}</a></p>
-                    @break
+                    @switch($file->file_type)
+                        @case('url')
+                            <p class="text-green-800 text-lg text-center"><a href="{{ $file->url }}" target="_blank"> {{ $file->alternative_name }}</a></p>
+                        @break
 
-                    @default
-                        <p class="text-green-800 text-lg text-center"><a href="{{ '/home/viewFile/'.$category->title.'/'.$file->id }}" target="_blank"> {{ $file->alternative_name }}</a></p>
-                @endswitch
+                        @default
+                            <p class="text-green-800 text-lg text-center"><a href="{{ '/home/viewFile/'.$category->title.'/'.$file->id }}" target="_blank"> {{ $file->alternative_name }}</a></p>
+                    @endswitch
+                </div>
             </div>
         </div>
     @endforeach
